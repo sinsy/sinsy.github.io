adff571506b758ebc384e25d289051c6e0df5a07
liujiabao <liujiabao@qihaiyun.com>
2020-05-30 17:52:05 +0800

    test js 提交

---
+++ public/js/test1.js
@@ -0,0 +1,159 @@
+const data1 = {
+  "requestID": "600884AF",
+  "version": "1.0",
+  "cmd": "print",
+  "task": {
+    "documents": [{
+      "contents": [{
+        "data": {
+          "goodsInfoList": [{
+            "goodsNo": "3103090004",
+            "quantity": "1",
+            "packageCode": "",
+            "goodsName": "晶致丝滑IPL光子脱毛仪 闪悦版（套装）",
+            "goodsProps": "14",
+            "goodsWeight": "0",
+            "barCode": "4573253672288"
+          }],
+          "receiverInfo": {
+            "receiverAddress": "淞南镇长江路180号欣遇公寓",
+            "receiverPhone": "17502575455",
+            "buyerNick": "322",
+            "receiverName": "叶赛莉",
+            "receiverMobile": "17502575455",
+            "destination": "021",
+            "receiverProvince": "上海市",
+            "receiverCounty": "宝山区",
+            "receiverCity": "上海市",
+            "receiverFullAddress": "上海市上海市宝山区淞南镇长江路180号欣遇公寓"
+          },
+          "orderInfo": {
+            "orderNo": "2005271733040178",
+            "tradeNo": "523052155362765",
+            "expressTypeLogo": "特惠专配",
+            "insureFlag": 1,
+            "printDate": "2020-05-28 00:00:04",
+            "deliveryNo": "2005271733040178",
+            "destRouteLabel": "021",
+            "goodsWeight": "0.0",
+            "orderVolume": "0",
+            "paymentType": "寄付月结",
+            "sourceCreateDate": "2020-05-27 17:33:04",
+            "zzList": [],
+            "logisticsNo": "SF1023574470215",
+            "goodsNum": "1",
+            "receiveAreaCode": "757",
+            "sourceCreateTime": "2020-05-27 17:33:04"
+          },
+          "shipperInfo": {
+            "shipperMobile": "18126230063",
+            "shipperCounty": "顺德区大良街道办事处",
+            "shipperProvince": "广东省",
+            "shipperPhone": "18126230063",
+            "shipperShopName": "小米优品",
+            "shipperName": "COSBEAUTY",
+            "shipperZipCode": "",
+            "shipperShopCode": "zjsc018",
+            "shipperCity": "佛山市",
+            "shipperAddress": "五沙社区(云仓)普洛斯二期",
+            "shipperFullAddress": "广东省佛山市顺德区大良街道办事处五沙社区(云仓)普洛斯二期",
+            "shipperStoreName": "顺德正品仓（打单）"
+          },
+          "logoInfo": {
+            "sfQrcode": "MMM={'k1':'021','k2':'021','k3':'','k4':'T77','k5':'SF1023574470215','k6':'A1','k7':'5dd167a0'}",
+            "abFlag": "A1",
+            "showLogo": false,
+            "zmdan": 0,
+            "logoImage": "",
+            "tradeNoCode": "523052155362765",
+            "codingMapping": "U28",
+            "printIcon": "00000000",
+            "imageSrc": "http://cdn-cloudprint.cainiao.com/waybill-print/cloudprint-imgs/f15f1bf0d9604b73ba423518c4de749f.png",
+            "projectName": "特惠专配",
+            "logisticsNoCode": "SF1023574470215",
+            "xbFlag": "0"
+          }
+        },
+        "templateURL": "http://cloudprint.cainiao.com/template/standard/343439/11"
+      }, {
+        "data": {
+          "goodsInfoList": [{
+            "goodsNo": "3103090004",
+            "quantity": "1",
+            "packageCode": "",
+            "goodsName": "晶致丝滑IPL光子脱毛仪 闪悦版（套装）",
+            "goodsProps": "14",
+            "goodsWeight": "0",
+            "barCode": "4573253672288"
+          }],
+          "receiverInfo": {
+            "receiverAddress": "淞南镇长江路180号欣遇公寓",
+            "receiverPhone": "17502575455",
+            "buyerNick": "322",
+            "receiverName": "叶赛莉",
+            "receiverMobile": "17502575455",
+            "destination": "021",
+            "receiverProvince": "上海市",
+            "receiverCounty": "宝山区",
+            "receiverCity": "上海市",
+            "receiverFullAddress": "上海市上海市宝山区淞南镇长江路180号欣遇公寓"
+          },
+          "orderInfo": {
+            "orderNo": "2005271733040178",
+            "tradeNo": "523052155362765",
+            "expressTypeLogo": "特惠专配",
+            "insureFlag": 1,
+            "printDate": "2020-05-28 00:00:04",
+            "deliveryNo": "2005271733040178",
+            "destRouteLabel": "021",
+            "goodsWeight": "0.0",
+            "orderVolume": "0",
+            "paymentType": "寄付月结",
+            "sourceCreateDate": "2020-05-27 17:33:04",
+            "zzList": [],
+            "logisticsNo": "SF1023574470215",
+            "goodsNum": "1",
+            "receiveAreaCode": "757",
+            "sourceCreateTime": "2020-05-27 17:33:04"
+          },
+          "shipperInfo": {
+            "shipperMobile": "18126230063",
+            "shipperCounty": "顺德区大良街道办事处",
+            "shipperProvince": "广东省",
+            "shipperPhone": "18126230063",
+            "shipperShopName": "小米优品",
+            "shipperName": "COSBEAUTY",
+            "shipperZipCode": "",
+            "shipperShopCode": "zjsc018",
+            "shipperCity": "佛山市",
+            "shipperAddress": "五沙社区(云仓)普洛斯二期",
+            "shipperFullAddress": "广东省佛山市顺德区大良街道办事处五沙社区(云仓)普洛斯二期",
+            "shipperStoreName": "顺德正品仓（打单）"
+          },
+          "logoInfo": {
+            "sfQrcode": "MMM={'k1':'021','k2':'021','k3':'','k4':'T77','k5':'SF1023574470215','k6':'A1','k7':'5dd167a0'}",
+            "abFlag": "A1",
+            "showLogo": false,
+            "zmdan": 0,
+            "logoImage": "",
+            "tradeNoCode": "523052155362765",
+            "codingMapping": "U28",
+            "printIcon": "00000000",
+            "imageSrc": "http://cdn-cloudprint.cainiao.com/waybill-print/cloudprint-imgs/f15f1bf0d9604b73ba423518c4de749f.png",
+            "projectName": "特惠专配",
+            "logisticsNoCode": "SF1023574470215",
+            "xbFlag": "0"
+          }
+        },
+        "templateURL": "http://erp.xiandadan.com/custom-wms/none/mdTemplate/316154931830710272"
+      }],
+      "documentID": "SF1023574470215"
+    }],
+    "firstDocumentNumber": 1,
+    "totalDocumentCount": 1,
+    "preview": false,
+    "notifyMode": "oneByone",
+    "taskID": "BDE838FB",
+    "printer": "迅捷PDF虚拟打印机"
+  }
+}
---
+++ public/js/test2.js
@@ -0,0 +1,159 @@
+const data = {
+  "requestID": "C7E9515F",
+  "version": "1.0",
+  "cmd": "print",
+  "task": {
+    "documents": [{
+      "contents": [{
+        "data": {
+          "goodsInfoList": [{
+            "goodsNo": "3103090004",
+            "quantity": "1",
+            "packageCode": "",
+            "goodsName": "晶致丝滑IPL光子脱毛仪 闪悦版（套装）",
+            "goodsProps": "14",
+            "goodsWeight": "0",
+            "barCode": "4573253672288"
+          }],
+          "receiverInfo": {
+            "receiverAddress": "横县横州镇宝华西路延长线横县幸福家园小区",
+            "receiverPhone": "18276184576",
+            "buyerNick": "323",
+            "receiverName": "黄铃",
+            "receiverMobile": "18276184576",
+            "destination": "771",
+            "receiverProvince": "广西壮族自治区",
+            "receiverCounty": "横县",
+            "receiverCity": "南宁市",
+            "receiverFullAddress": "广西壮族自治区南宁市横县横县横州镇宝华西路延长线横县幸福家园小区"
+          },
+          "orderInfo": {
+            "orderNo": "2005271733030076",
+            "tradeNo": "590031096654475",
+            "expressTypeLogo": "特惠专配",
+            "insureFlag": 1,
+            "printDate": "2020-05-28 00:18:13",
+            "deliveryNo": "2005271733030076",
+            "destRouteLabel": "771AG",
+            "goodsWeight": "0.0",
+            "orderVolume": "0",
+            "paymentType": "寄付月结",
+            "sourceCreateDate": "2020-05-27 17:33:03",
+            "zzList": [],
+            "logisticsNo": "SF1023574626292",
+            "goodsNum": "1",
+            "receiveAreaCode": "757",
+            "sourceCreateTime": "2020-05-27 17:33:03"
+          },
+          "shipperInfo": {
+            "shipperMobile": "18126230063",
+            "shipperCounty": "顺德区大良街道办事处",
+            "shipperProvince": "广东省",
+            "shipperPhone": "18126230063",
+            "shipperShopName": "小米优品",
+            "shipperName": "COSBEAUTY",
+            "shipperZipCode": "",
+            "shipperShopCode": "zjsc018",
+            "shipperCity": "佛山市",
+            "shipperAddress": "五沙社区(云仓)普洛斯二期",
+            "shipperFullAddress": "广东省佛山市顺德区大良街道办事处五沙社区(云仓)普洛斯二期",
+            "shipperStoreName": "顺德正品仓（打单）"
+          },
+          "logoInfo": {
+            "sfQrcode": "MMM={'k1':'771W','k2':'771AG','k3':'009','k4':'T77','k5':'SF1023574626292','k6':'','k7':'d5abfe9c'}",
+            "destTeamCode": "009",
+            "showLogo": false,
+            "zmdan": 0,
+            "logoImage": "",
+            "tradeNoCode": "590031096654475",
+            "codingMapping": "U28",
+            "printIcon": "00000000",
+            "imageSrc": "http://cdn-cloudprint.cainiao.com/waybill-print/cloudprint-imgs/f15f1bf0d9604b73ba423518c4de749f.png",
+            "projectName": "特惠专配",
+            "logisticsNoCode": "SF1023574626292",
+            "xbFlag": "0"
+          }
+        },
+        "templateURL": "http://cloudprint.cainiao.com/template/standard/343439/11"
+      }, {
+        "data": {
+          "goodsInfoList": [{
+            "goodsNo": "3103090004",
+            "quantity": "1",
+            "packageCode": "",
+            "goodsName": "晶致丝滑IPL光子脱毛仪 闪悦版（套装）",
+            "goodsProps": "14",
+            "goodsWeight": "0",
+            "barCode": "4573253672288"
+          }],
+          "receiverInfo": {
+            "receiverAddress": "横县横州镇宝华西路延长线横县幸福家园小区",
+            "receiverPhone": "18276184576",
+            "buyerNick": "323",
+            "receiverName": "黄铃",
+            "receiverMobile": "18276184576",
+            "destination": "771",
+            "receiverProvince": "广西壮族自治区",
+            "receiverCounty": "横县",
+            "receiverCity": "南宁市",
+            "receiverFullAddress": "广西壮族自治区南宁市横县横县横州镇宝华西路延长线横县幸福家园小区"
+          },
+          "orderInfo": {
+            "orderNo": "2005271733030076",
+            "tradeNo": "590031096654475",
+            "expressTypeLogo": "特惠专配",
+            "insureFlag": 1,
+            "printDate": "2020-05-28 00:18:13",
+            "deliveryNo": "2005271733030076",
+            "destRouteLabel": "771AG",
+            "goodsWeight": "0.0",
+            "orderVolume": "0",
+            "paymentType": "寄付月结",
+            "sourceCreateDate": "2020-05-27 17:33:03",
+            "zzList": [],
+            "logisticsNo": "SF1023574626292",
+            "goodsNum": "1",
+            "receiveAreaCode": "757",
+            "sourceCreateTime": "2020-05-27 17:33:03"
+          },
+          "shipperInfo": {
+            "shipperMobile": "18126230063",
+            "shipperCounty": "顺德区大良街道办事处",
+            "shipperProvince": "广东省",
+            "shipperPhone": "18126230063",
+            "shipperShopName": "小米优品",
+            "shipperName": "COSBEAUTY",
+            "shipperZipCode": "",
+            "shipperShopCode": "zjsc018",
+            "shipperCity": "佛山市",
+            "shipperAddress": "五沙社区(云仓)普洛斯二期",
+            "shipperFullAddress": "广东省佛山市顺德区大良街道办事处五沙社区(云仓)普洛斯二期",
+            "shipperStoreName": "顺德正品仓（打单）"
+          },
+          "logoInfo": {
+            "sfQrcode": "MMM={'k1':'771W','k2':'771AG','k3':'009','k4':'T77','k5':'SF1023574626292','k6':'','k7':'d5abfe9c'}",
+            "destTeamCode": "009",
+            "showLogo": false,
+            "zmdan": 0,
+            "logoImage": "",
+            "tradeNoCode": "590031096654475",
+            "codingMapping": "U28",
+            "printIcon": "00000000",
+            "imageSrc": "http://cdn-cloudprint.cainiao.com/waybill-print/cloudprint-imgs/f15f1bf0d9604b73ba423518c4de749f.png",
+            "projectName": "特惠专配",
+            "logisticsNoCode": "SF1023574626292",
+            "xbFlag": "0"
+          }
+        },
+        "templateURL": "http://erp.xiandadan.com/custom-wms/none/mdTemplate/316154931830710272"
+      }],
+      "documentID": "SF1023574626292"
+    }],
+    "firstDocumentNumber": 1,
+    "totalDocumentCount": 1,
+    "preview": false,
+    "notifyMode": "oneByone",
+    "taskID": "20A441B2",
+    "printer": "迅捷PDF虚拟打印机"
+  }
+}
